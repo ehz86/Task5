4371e122378b29a2f7a1cce22e2d4b9aa26e7a3d
Egor <91316084+ehz86@users.noreply.github.com>
2021-10-04 18:54:58 +0300

    Add files via upload

--- p5.py
+++ p5.py
@@ -1,8 +1,22 @@
-a = int(input ("Введите 5 значное число: "))
-if  (a>9999) and (a<100000):
-	if(a // 10000 == a % 10)and((a // 1000) % 10 == (a % 100) // 10):
-		print ("Yes")
+flag = True
+flag2 = True
+while flag:
+	try:
+		a = int(input ("Введите 5 значное число: "))
+	except Exception:
+		print ("Invalid data")
 	else:
-		print ("No")
-else:
-	print ("Invalid data")
+		flag = False
+while flag2:
+	try: 
+		if  (a>9999) and (a<100000):
+		    if(a // 10000 == a % 10)and((a // 1000) % 10 == (a % 100) // 10):
+		    	print("Yes")
+		    else:
+		    	print("No")	
+		else:
+			print("Invalid data")
+	except:
+		print("Invalid data")
+	else:
+		flag2 = False
